667df37a2a4dcb5140d52a30a5ee5bf3954943a7
Daniel Deiana <d.deiana1@studenti.unipi.it>
2023-01-17 16:13:53 +0100

    refactor

--- demo/src/main/java/com/example/demo/Controller/UserController.java
+++ demo/src/main/java/com/example/demo/Controller/UserController.java
@@ -1,12 +1,16 @@
 package com.example.demo.Controller;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class UserController {
 
-		@GetMapping("/")
-		String getUser() {
-				return "people";
+		@GetMapping
+		public String getUser(Model model) {
+			model.addAttribute("something", "value of attribute something");
+			return "people";
 		}
 }
--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,5 +8,4 @@ public class DemoApplication {
 		public static void main(String[] args) {
 				SpringApplication.run(DemoApplication.class, args);
 		}
-
 }
