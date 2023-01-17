5a5baa664db0b3a899f83cee0eb8328a63b325b6
Daniel Deiana <d.deiana1@studenti.unipi.it>
2023-01-17 16:32:45 +0100

    Thymeleaf works

--- demo/src/main/java/com/example/demo/Controller/UserController.java
+++ demo/src/main/java/com/example/demo/Controller/UserController.java
@@ -2,15 +2,12 @@ package com.example.demo.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class UserController {
-
 		@GetMapping
-		public String getUser(Model model) {
+		public String userPage(Model model) {
 			model.addAttribute("something", "value of attribute something");
-			return "people";
+			return "user";
 		}
 }
