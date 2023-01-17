9e378c994ff309f458e32325e2d1412f22a0ed19
Daniel Deiana <d.deiana1@studenti.unipi.it>
2023-01-17 15:49:21 +0100

    testing request mapping

---
+++ demo/src/main/java/com/example/demo/Controller/UserController.java
@@ -0,0 +1,12 @@
+package com.example.demo.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+		@GetMapping("/")
+		String getUser() {
+				return "people";
+		}
+}
