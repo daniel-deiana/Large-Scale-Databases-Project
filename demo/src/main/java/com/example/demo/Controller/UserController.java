c3ac0a915b5fa685468a895b97e4cd600b856643
Daniel Deiana <d.deiana1@studenti.unipi.it>
2023-01-18 18:02:29 +0100

    Added api layer directories

--- demo/src/main/java/com/example/demo/Controller/UserController.java
+++ demo/src/main/java/com/example/demo/Controller/UserController.java
@@ -2,10 +2,11 @@ package com.example.demo.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
-		@GetMapping
+		@RequestMapping("/")
 		public String userPage(Model model) {
 			model.addAttribute("something", "value of attribute something");
 			return "user";
