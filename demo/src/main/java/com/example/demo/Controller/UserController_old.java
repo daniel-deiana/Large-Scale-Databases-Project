7a21139bf82057350d7224bc454f65523d6524b4
marckgt7 <marcogalante2000@yahoo.it>
2023-01-31 18:28:53 +0100

    html_controllori

--- demo/src/main/java/com/example/demo/Controller/UserController_old.java
+++ demo/src/main/java/com/example/demo/Controller/UserController_old.java
@@ -3,7 +3,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+/*
 @Controller
 public class UserController {
 		@RequestMapping("/")
@@ -12,3 +12,4 @@ public class UserController {
 			return "index";
 		}
 }
+*/
