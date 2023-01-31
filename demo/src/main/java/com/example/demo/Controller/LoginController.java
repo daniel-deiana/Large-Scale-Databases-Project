c5a9f37234dabca0019a9ed7e44a6b09f2ff5657
marckgt7 <marcogalante2000@yahoo.it>
2023-01-31 19:42:26 +0100

    html

--- demo/src/main/java/com/example/demo/Controller/LoginController.java
+++ demo/src/main/java/com/example/demo/Controller/LoginController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 		@GetMapping("/login")
-		public String loginPage(){
+		public String login(){
 						return "login";
 		}
 }
