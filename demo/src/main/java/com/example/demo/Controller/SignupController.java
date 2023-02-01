737031911cff98f54c125d20da539bada85026f5
Driveights <lorenzoguidotti21@gmail.com>
2023-02-01 19:43:56 +0100

    Login/Signup

--- demo/src/main/java/com/example/demo/Controller/SignupController.java
+++ demo/src/main/java/com/example/demo/Controller/SignupController.java
@@ -1,6 +1,7 @@
 package com.example.demo.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SignupController {
--- demo/src/main/resources/static/js/signup.js
+++ demo/src/main/resources/static/js/signup.js
@@ -1,8 +1,13 @@
 $(document).ready(function () {
     document.getElementById('signup_button').onclick = function (e) {
         console.log("start")
+        let username_  = document.getElementById("username").value
+        let date_     = document.getElementById("date").value
+        let gender_    = document.getElementById("gender").value
+        let password_  = document.getElementById("floatingPassword").value
         $.ajax({
                     url : "/api/signup",
+                    data : {username: username_, date: date_, gender:gender_, password: password_},
                     dataType : 'json',
                     method : "get",
                     success: function(data) {
