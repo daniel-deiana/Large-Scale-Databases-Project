2dae8d5c4644f3f7e95d68af4bf8c057ad8bbf9f
marckgt7 <marcogalante2000@yahoo.it>
2023-02-06 15:27:23 +0100

    hd

--- demo/src/main/java/com/example/demo/Controller/api/SignUpController.java
+++ demo/src/main/java/com/example/demo/Controller/api/SignUpController.java
@@ -3,6 +3,7 @@ package com.example.demo.Controller.api;
 import com.example.demo.Model.User;
 import com.example.demo.Service.UserService;
 import com.google.common.hash.Hashing;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,12 +25,13 @@ public class SignUpController {
 												 @RequestParam(value = "country") String country,
 												 @RequestParam(value = "password") String password) {
 
-			String hashed = Hashing.sha256()
+		Gson gson = new Gson();
+		String hashed = Hashing.sha256()
 							.hashString(password, StandardCharsets.UTF_8)
 							.toString();
 			User user = new User(username,gender,date, country ,hashed);
 			userService.addUser(user);
-			return "ok";
+			return gson.toJson("{\"type\": 0, \"message\" : \"ok\"}");
 	}
 
 }
