ec23b34bc7f78ddf1a77c5cc6fe7d16178e00cd8
marckgt7 <marcogalante2000@yahoo.it>
2023-02-06 15:58:24 +0100

    Merge remote-tracking branch 'upstream/main'

--- demo/src/main/java/com/example/demo/Controller/api/SignUpController.java
+++ demo/src/main/java/com/example/demo/Controller/api/SignUpController.java
@@ -2,6 +2,7 @@ package com.example.demo.Controller.api;
 
 import com.example.demo.Model.User;
 import com.example.demo.Service.UserService;
+import com.example.demo.Utilities.SVariables;
 import com.google.common.hash.Hashing;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +10,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
+@SessionAttributes("sessionVariables")
 @RestController
 public class SignUpController {
 	@Autowired
@@ -31,6 +34,11 @@ public class SignUpController {
 							.toString();
 			User user = new User(username,gender,date, country ,hashed);
 			userService.addUser(user);
+		if(model.getAttribute("sessionVariables") == null)
+			model.addAttribute("sessionVariables", new SVariables());
+			SVariables sv = (SVariables) model.getAttribute("sessionVariables");
+			sv.myself = user.getUsername();
+			model.addAttribute("sessionVariables",sv);
 			return gson.toJson("{\"type\": 0, \"message\" : \"ok\"}");
 	}
 
