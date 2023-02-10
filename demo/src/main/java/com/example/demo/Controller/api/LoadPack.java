8f63937f91cdfbc7b98f456f861f902287890307
Driveights <lorenzoguidotti21@gmail.com>
2023-02-10 18:15:42 +0100

    più cose

--- demo/src/main/java/com/example/demo/Controller/api/LoadPack.java
+++ demo/src/main/java/com/example/demo/Controller/api/LoadPack.java
@@ -28,7 +28,19 @@ public class LoadPack {
         Gson gson = new Gson();
         List<FigureDTO> pos_figures;
 
-        pos_figures = userService.getReviewedFigures(sv.myself);
+        pos_figures = userService.getReviewedFigures(sv.myself,3);
+        userService.addHasCharacter(sv.myself, pos_figures);
+        return gson.toJson(pos_figures);
+
+    }
+
+    @RequestMapping("/api/PossibleCards")
+    public @ResponseBody String possibleCards(Model model) {
+        SVariables sv = (SVariables) model.getAttribute("sessionVariables");
+        Gson gson = new Gson();
+        List<FigureDTO> pos_figures;
+
+        pos_figures = userService.getReviewedFigures(sv.myself,10);
         userService.addHasCharacter(sv.myself, pos_figures);
         return gson.toJson(pos_figures);
 
--- demo/src/main/resources/static/js/shop.js
+++ demo/src/main/resources/static/js/shop.js
@@ -4,16 +4,34 @@ $(document).ready(function () {
         paste_button = document.getElementById("paste_button")
         init_div.style.display = 'None'
         $.ajax({
-                            url : "/api/LoadPack",
-                            method : "get",
-                            success: function(data) {
-                                result = jQuery.parseJSON(data)
-                                for(fig in result){
-                                    let html = '<div style= "display: flex; flex-direction: column; align-items: center; justify-content: center;"><img src="img/unkown_c.png" class="img-fluid animated" alt="" style="width = 80%; height= 80%;"><h2>' + result[fig].name +'</h2></div>'
-                                    $('#pack').append(html)
-                                }
-                                paste_button.style.display=""
-                            }
-                        })
-    }
+                url : "/api/LoadPack",
+                method : "get",
+                success: function(data) {
+                    result = jQuery.parseJSON(data)
+                    for(fig in result){
+                        let html = '<div style= "display: flex; flex-direction: column; align-items: center; justify-content: center;"><img src="img/unkown_c.png" class="img-fluid animated" alt="" style="width = 80%; height= 80%;"><h2>' + result[fig].name +'</h2></div>'
+                        $('#pack').append(html)
+                    }
+                    paste_button.style.display=""
+                }
+            })
+        }
+
+    $.ajax({
+            url : "/api/PossibleCards",
+            method : "get",
+            success: function(data) {
+                result = jQuery.parseJSON(data)
+                console.log(result)
+                for(fig in result){
+                    let html = '<div class="swiper-slide">' +
+                        '<div class="testimonial-item" style="display: flex; flex-direction: column; align-items: center">' +
+                        '<h3 style=" cursor: pointer; text-align: center; margin: auto;">'+ result[fig].name +'</h3>\n' +
+                        '<img src="../img/unkown_c.png" class="testimonial-img" alt="">' +
+                        '</div>' +
+                        '</div>'
+                    $('#swiper').append(html)
+                }
+            }
+        })
 });
