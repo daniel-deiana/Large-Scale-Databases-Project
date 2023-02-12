dc175e421193288e197f266f81fadc5325cccefd
Driveights <lorenzoguidotti21@gmail.com>
2023-02-12 10:34:37 +0100

    login admin

--- demo/src/main/java/com/example/demo/Service/AnimeService.java
+++ demo/src/main/java/com/example/demo/Service/AnimeService.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
